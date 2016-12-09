ac249248310a9b35725211c6f50fdbcab639a08a
bolorundurowb <ogatimo@gmail.com>
2016-12-09 06:41:11 +0100

    feature: switch http request from library to plain request

--- app/src/main/java/com/inveniotechnologies/neophyte/Home.java
+++ app/src/main/java/com/inveniotechnologies/neophyte/Home.java
@@ -44,6 +44,7 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
