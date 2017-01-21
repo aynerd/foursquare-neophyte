a43ceafe77432f2c2a4672928f19fde92f6f09ae
bolorundurowb <ogatimo@gmail.com>
2017-01-21 11:37:09 +0100

    chore: restructures the details activity to be more modular

--- app/src/main/java/com/inveniotechnologies/neophyte/ui/Details.java
+++ app/src/main/java/com/inveniotechnologies/neophyte/ui/Details.java
@@ -16,29 +16,69 @@ import com.google.firebase.database.ValueEventListener;
 import com.inveniotechnologies.neophyte.R;
 import com.inveniotechnologies.neophyte.network.models.Record;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class Details extends AppCompatActivity {
-    private TextView lbl_title;
-    private TextView lbl_full_name;
-    private TextView lbl_age_group;
-    private TextView lbl_comments;
-    private TextView lbl_home_address;
-    private TextView lbl_home_tel;
-    private TextView lbl_mobile;
-    private TextView lbl_office_tel;
-    private TextView lbl_email;
-    private TextView lbl_how_you_found_us;
-    private TextView lbl_birthday;
-    private TextView lbl_spiritual_rebirth_date;
-    //
-    private TextView lbl_commit_life;
-    private TextView lbl_renew_commitment;
-    private TextView lbl_be_baptized;
-    private TextView lbl_talk_pastorate;
-    private TextView lbl_become_member;
-    private TextView lbl_discover_maturity;
-    private TextView lbl_discover_ministry;
-    //
-    private Button btn_close;
+    @BindView(R.id.lbl_title)
+    TextView lbl_title;
+
+    @BindView(R.id.lbl_full_name)
+    TextView lbl_full_name;
+
+    @BindView(R.id.lbl_age_group)
+    TextView lbl_age_group;
+
+    @BindView(R.id.lbl_comments)
+    TextView lbl_comments;
+
+    @BindView(R.id.lbl_home_address)
+    TextView lbl_home_address;
+
+    @BindView(R.id.lbl_home_tel)
+    TextView lbl_home_tel;
+
+    @BindView(R.id.lbl_mobile)
+    TextView lbl_mobile;
+
+    @BindView(R.id.lbl_office_tel)
+    TextView lbl_office_tel;
+
+    @BindView(R.id.lbl_email_address)
+    TextView lbl_email;
+
+    @BindView(R.id.lbl_how_you_found_us)
+    TextView lbl_how_you_found_us;
+
+    @BindView(R.id.lbl_birthday)
+    TextView lbl_birthday;
+
+    @BindView(R.id.lbl_spiritual_rebirth_date)
+    TextView lbl_spiritual_rebirth_date;
+
+    @BindView(R.id.lbl_commit_life)
+    TextView lbl_commit_life;
+
+    @BindView(R.id.lbl_renew_commitment)
+    TextView lbl_renew_commitment;
+
+    @BindView(R.id.lbl_be_baptized)
+    TextView lbl_be_baptized;
+
+    @BindView(R.id.lbl_talk_pastorate)
+    TextView lbl_talk_pastorate;
+
+    @BindView(R.id.lbl_become_member)
+    TextView lbl_become_member;
+
+    @BindView(R.id.lbl_discover_maturity)
+    TextView lbl_discover_maturity;
+
+    @BindView(R.id.lbl_discover_ministry)
+    TextView lbl_discover_ministry;
+
+    @BindView(R.id.btn_close)
+    Button btn_close;
     //
     private FirebaseDatabase database;
 
@@ -47,32 +87,12 @@ public class Details extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         //
+        ButterKnife.bind(this);
+        //
         Intent intent = getIntent();
         String date = intent.getStringExtra("date");
         String Uid = intent.getStringExtra("Uid");
         //
-        lbl_full_name = (TextView) findViewById(R.id.lbl_full_name);
-        lbl_home_address = (TextView) findViewById(R.id.lbl_home_address);
-        lbl_home_tel = (TextView) findViewById(R.id.lbl_home_tel);
-        lbl_office_tel = (TextView) findViewById(R.id.lbl_office_tel);
-        lbl_mobile = (TextView) findViewById(R.id.lbl_mobile);
-        lbl_email = (TextView) findViewById(R.id.lbl_email_address);
-        lbl_how_you_found_us = (TextView) findViewById(R.id.lbl_how_you_found_us);
-        lbl_comments = (TextView) findViewById(R.id.lbl_comments);
-        lbl_age_group = (TextView) findViewById(R.id.lbl_age_group);
-        lbl_title = (TextView) findViewById(R.id.lbl_title);
-        lbl_birthday = (TextView) findViewById(R.id.lbl_birthday);
-        lbl_spiritual_rebirth_date = (TextView) findViewById(R.id.lbl_spiritual_rebirth_date);
-        //
-        lbl_be_baptized = (TextView) findViewById(R.id.lbl_be_baptized);
-        lbl_become_member = (TextView) findViewById(R.id.lbl_become_member);
-        lbl_commit_life = (TextView) findViewById(R.id.lbl_commit_life);
-        lbl_discover_maturity = (TextView) findViewById(R.id.lbl_discover_maturity);
-        lbl_discover_ministry = (TextView) findViewById(R.id.lbl_discover_ministry);
-        lbl_renew_commitment = (TextView) findViewById(R.id.lbl_renew_commitment);
-        lbl_talk_pastorate = (TextView) findViewById(R.id.lbl_talk_pastorate);
-        //
-        btn_close = (Button) findViewById(R.id.btn_close);
         btn_close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -88,56 +108,55 @@ public class Details extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Record record = dataSnapshot.getValue(Record.class);
-                if(record != null) {
-                    lbl_birthday.setText(record.getBirthDay());
-                    lbl_full_name.setText(record.getFullName());
-                    lbl_comments.setText(record.getComments());
-                    lbl_title.setText(record.getTitle());
-                    lbl_age_group.setText(record.getAgeGroup());
-                    lbl_email.setText(record.getEmail());
-                    lbl_home_address.setText(record.getHomeAddress());
-                    lbl_home_tel.setText(record.getHomeTel());
-                    lbl_mobile.setText(record.getMobile());
-                    lbl_office_tel.setText(record.getOfficeTel());
-                    lbl_how_you_found_us.setText(record.getInvitedBy());
-                    lbl_spiritual_rebirth_date.setText(record.getDateOfSpiritualRebirth());
-                    //
-                    String[] decisions = record.getDecisions().split(";");
-                    for(int i = 0; i < decisions.length; i++) {
-                        String decision = decisions[i];
-                        if(decision.contains("pastorate")) {
-                            lbl_talk_pastorate.setVisibility(View.VISIBLE);
-                        }
-                        else if(decision.contains("maturity")) {
-                            lbl_discover_maturity.setVisibility(View.VISIBLE);
-                        }
-                        else if(decision.contains("ministry")) {
-                            lbl_discover_ministry.setVisibility(View.VISIBLE);
-                        }
-                        else if(decision.contains("member")) {
-                            lbl_become_member.setVisibility(View.VISIBLE);
-                        }
-                        else if(decision.contains("commitment")) {
-                            lbl_renew_commitment.setVisibility(View.VISIBLE);
-                        }
-                        else if(decision.contains("life")) {
-                            lbl_commit_life.setVisibility(View.VISIBLE);
-                        }
-                        else if(decision.contains("baptized")) {
-                            lbl_be_baptized.setVisibility(View.VISIBLE);
-                        }
-                    }
-                }
+                writeRecordToUi(record);
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 Toast.makeText(
                         Details.this,
-                        "An error occurred and the app has to close.",
-                        Toast.LENGTH_SHORT).show();
+                        "An error occurred and the details could not be retrieved."
+                                + "This activity has to close.",
+                        Toast.LENGTH_LONG).show();
                 finish();
             }
         });
+    }
+
+    private void writeRecordToUi(Record record) {
+        if (record != null) {
+            lbl_birthday.setText(record.getBirthDay());
+            lbl_full_name.setText(record.getFullName());
+            lbl_comments.setText(record.getComments());
+            lbl_title.setText(record.getTitle());
+            lbl_age_group.setText(record.getAgeGroup());
+            lbl_email.setText(record.getEmail());
+            lbl_home_address.setText(record.getHomeAddress());
+            lbl_home_tel.setText(record.getHomeTel());
+            lbl_mobile.setText(record.getMobile());
+            lbl_office_tel.setText(record.getOfficeTel());
+            lbl_how_you_found_us.setText(record.getInvitedBy());
+            lbl_spiritual_rebirth_date.setText(record.getDateOfSpiritualRebirth());
+            //
+            String[] decisions = record.getDecisions().split(";");
+            for (int i = 0; i < decisions.length; i++) {
+                String decision = decisions[i];
+                if (decision.contains("pastorate")) {
+                    lbl_talk_pastorate.setVisibility(View.VISIBLE);
+                } else if (decision.contains("maturity")) {
+                    lbl_discover_maturity.setVisibility(View.VISIBLE);
+                } else if (decision.contains("ministry")) {
+                    lbl_discover_ministry.setVisibility(View.VISIBLE);
+                } else if (decision.contains("member")) {
+                    lbl_become_member.setVisibility(View.VISIBLE);
+                } else if (decision.contains("commitment")) {
+                    lbl_renew_commitment.setVisibility(View.VISIBLE);
+                } else if (decision.contains("life")) {
+                    lbl_commit_life.setVisibility(View.VISIBLE);
+                } else if (decision.contains("baptized")) {
+                    lbl_be_baptized.setVisibility(View.VISIBLE);
+                }
+            }
+        }
     }
 }
