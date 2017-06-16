52db402895efa960198723549759258aa839f059
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-06-16 16:07:18 +0100

    issue #35 | adds initial client connection code

--- app/src/main/java/com/inveniotechnologies/neophyte/network/util/Uploader.java
+++ app/src/main/java/com/inveniotechnologies/neophyte/network/util/Uploader.java
@@ -1,9 +1,45 @@
 package com.inveniotechnologies.neophyte.network.util;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
+
 /**
  * Created by winner-timothybolorunduro on 16/06/2017.
  */
 
-public class Uploader {
+public class Uploader implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private final String TAG = "Neophyte: ";
+    private GoogleApiClient mGoogleApiClient;
 
+    public Uploader(Context context) {
+        mGoogleApiClient = new GoogleApiClient.Builder(context)
+                .addApi(Drive.API)
+                .addScope(Drive.SCOPE_FILE)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "GoogleApiClient connection suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
