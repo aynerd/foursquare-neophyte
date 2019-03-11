bb97b8e09209d01d20d47bb0e18814e89f0d71c0
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2019-03-11 16:24:32 +0100

    #92 | add in the view models for the list page

--- foursquare-neophyte/foursquare-neophyte/Views/HomePage.xaml.cs
+++ foursquare-neophyte/foursquare-neophyte/Views/HomePage.xaml.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Firebase.Database;
+using Firebase.Database.Query;
+using foursquare_neophyte.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,11 +12,52 @@ namespace foursquare_neophyte.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
+        private static string daysNodeUrl = $"{Constants.FirebaseUrl}";
+        ObservableCollection<DateDisplayModel> Dates = new ObservableCollection<DateDisplayModel>();
+        FirebaseClient firebaseClient = new FirebaseClient(daysNodeUrl);
+
+
         public HomePage()
         {
             InitializeComponent();
 
             Title = "Foursquare Newcomers";
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                absContent.BackgroundColor = Color.FromHex("#FAFAFA");
+            }
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // clear existing data
+            Dates.Clear();
+
+            var dates = firebaseClient
+                .Child("members")
+                .OrderByKey()
+                .LimitToLast(25)
+                .OnceAsync<object>()
+                .Result
+                .OrderByDescending(x => x.Key)
+                .Select(x => x.Key); ;
+
+            foreach (var date in dates)
+            {
+                var displayModel = new DateDisplayModel();
+                displayModel.Date = date;
+                displayModel.DateDetails = (DateTime.Parse(date)).ToString("ddd, dd MMM yyy");
+                Dates.Add(displayModel);
+            }
+
+            this.lstDates.ItemsSource = Dates;
+        }
+
+        protected async void AddRecord(object sender, EventArgs e)
+        {
+
         }
     }
 }
