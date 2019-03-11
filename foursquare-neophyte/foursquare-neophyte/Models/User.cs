f733b1d8ae3041822bb25ce26df568e817320dfe
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2019-03-11 15:26:23 +0100

    #92 | add the user model

---
+++ foursquare-neophyte/foursquare-neophyte/Models/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace foursquare_neophyte.Models
+{
+    public class User
+    {
+        public string AgeGroup { get; set; }
+
+
+        public string Birthday { get; set; }
+
+
+        public string Comments { get; set; }
+
+
+        public string Decisions { get; set; }
+
+
+        public string Email { get; set; }
+
+
+        public string FullName { get; set; }
+
+
+        public string HomeAddress { get; set; }
+
+
+        public string HomeTel { get; set; }
+
+
+        public string InvitedBy { get; set; }
+
+
+        public string Mobile { get; set; }
+
+
+        public string OfficeTel { get; set; }
+
+
+        public string Title { get; set; }
+
+
+        public User()
+        {
+        }
+    }
+}
